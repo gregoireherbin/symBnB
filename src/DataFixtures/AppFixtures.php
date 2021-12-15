6737d8cb77af08dc687646980d43acddd953c777
Gregoire HERBIN <melozy@free.fr>
2021-12-15 03:25:02 +0100

    Base de donnée & Fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ad;
+use Faker\Factory;
+use App\Entity\Image;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {   
+          $faker= Factory::create('FR-fr');
+        
+            for ($i=1;$i<=30;$i++){
+               
+            $ad = new Ad();
+
+            $title = $faker ->sentence();
+            $content = '<p>'.join('<p></p>', $faker -> paragraphs(5)).'</p>';
+            $introduction = $faker -> paragraph (2);
+            $coverImage = $faker -> imageUrl($width = 1000, $height = 350);
+               
+            $ad->setTitle($title)
+                ->setContent($content)
+                ->setIntroduction($introduction)
+                ->setPrice(mt_rand(20,300))
+                ->setRooms(mt_rand(1,5))
+                ->setCoverImage($coverImage);
+            
+                
+                        
+                for ($j=1;$j<=mt_rand(2,5);$j++){
+
+                    $image = new Image();
+
+                    $image->setUrl($faker->imageUrl())
+                        ->setAd($ad) 
+                        ->setCaption($faker->sentence());  
+                    
+                    $manager->persist($image);
+                        }
+
+                        $manager->persist($ad); 
+                    
+                    }
+
+
+                $manager->flush();
+     }
+
+            
+}
